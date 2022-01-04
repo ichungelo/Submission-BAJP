289676304ed376a0a13ba4beb9ff6a3a80735271
Krisna Satriadi <krisna.s.362@gmail.com>
2022-01-04 14:06:21 +0800

    build activity_detail.xml page

--- app/src/main/java/com/ichungelo/catfilm/model/DataEntity.kt
+++ app/src/main/java/com/ichungelo/catfilm/model/DataEntity.kt
@@ -1,5 +1,9 @@
 package com.ichungelo.catfilm.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class DataEntity(
     var dataId: String,
     var title: String,
@@ -9,4 +13,4 @@ data class DataEntity(
     var tagline: String,
     var overview: String,
     var poster: Int
-)
+): Parcelable
--- app/src/main/java/com/ichungelo/catfilm/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/ichungelo/catfilm/ui/detail/DetailActivity.kt
@@ -2,18 +2,28 @@ package com.ichungelo.catfilm.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.ichungelo.catfilm.R
 import com.ichungelo.catfilm.databinding.ActivityDetailBinding
 import com.ichungelo.catfilm.databinding.ActivityMainBinding
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnDetailBack.setOnClickListener(this)
     }
 
     companion object {
         const val EXTRA_DATA = "extra_data"
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_detail_back -> onBackPressed()
+        }
     }
 }
